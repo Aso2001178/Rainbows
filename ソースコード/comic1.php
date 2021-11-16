6e2cd8a97d5a080e3d5323b448919207119f87bb
Aso2001178 <83046790+Aso2001178@users.noreply.github.com>
2021-11-16 14:36:18 +0900

    Add files via upload

---
+++ ソースコード/comic1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>toppage</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap">
+    <link rel="stylesheet" href="./css/comicstyle.css">
+</head>
+<body>
+<div class="container">
+    <div id="parent">
+        <div id="child1">
+            <header>
+                <div class="icon"><img src="img/本アイコン.png" width="75" height="75"></div>
+                <div class="title"><h1>comic store</h1></div>
+            </header>
+
+
+            <div class="link_btn">
+                <div class="btn2"><a href="register.php" >会員登録はこちら！</a></div>
+            </div>
+
+
+            <div class="login">
+                //ログイン機能
+            </div>
+
+            <div class="search">
+
+            </div>
+            <div class="btn">
+                <a href="cart.php" class="button">カート</a><br>
+                <a href="memberinfo.php" class="button">登録情報</a><br>
+                <a href="history.php" class="button">購入履歴</a><br>
+                <a href="withdraw.php" class="button">退会</a><br>
+                <a href="toppage.php" class="button">トップページ</a>
+            </div>
+        </div>
+        <div id="child2">
+            <div class="comicimg"><img src="img/ワンピース.png" width="260" height="386"></div><br>
+            <div class="comictitle"><h2>ONEPIECE</h2></div><br>
+            <div class="comicprice"><h2>￥５００</h2></div><br>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
