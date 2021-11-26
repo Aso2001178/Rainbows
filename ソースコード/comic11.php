7f4b7990482a1d635e7e0cfc827ac563b95a79cf
Aso2001174 <83046992+Aso2001174@users.noreply.github.com>
2021-11-26 11:59:40 +0900

    Add files via upload

---
+++ ソースコード/comic11.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>転生したらスライムだった件</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&family=Russo+One&display=swap">
+    <link rel="stylesheet" href="./css/comicstyle.css">
+    <link rel="stylesheet" href="./css/selectstyle.css">
+</head>
+<body>
+<div class="container">
+    <div id="parent">
+        <div id="child1">
+            <header>
+                <div class="icon"><img src="img/本アイコン.png" width="75" height="75"></div>
+                <div class="title"><h1>comic store</h1></div>
+            </header>
+            <div class="link_btn">
+                <div class="btn2"><a href="register.php" >会員登録はこちら！</a></div>
+            </div>
+            <div class="login">
+                //ログイン機能
+            </div>
+            <div class="search">
+            </div>
+            <div class="btn">
+                <a href="cart.php" class="button">カート</a><br>
+                <a href="memberinfo.php" class="button">登録情報</a><br>
+                <a href="history.php" class="button">購入履歴</a><br>
+                <a href="withdraw.php" class="button">退会</a><br>
+                <a href="toppage.php" class="button">トップページ</a>
+            </div>
+        </div>
+        <div id="child2">
+            <div class="comicimg"><img src="img/転生したらスライムだった件.png" width="260" height="386"></div><br>
+            <?php
+            $pdo = new PDO('mysql:host=mysql147.phy.lolipop.lan;
+                dbname=LAA1291127-rainbows;charset=utf8',
+                'LAA1291127',
+                '1234123');
+            foreach($pdo->query('SELECT item_code, item_name, price FROM m_items WHERE item_code = 11') as $row){
+                echo '<form action="cart-insert.php" method="post">';
+
+                echo '<div class="comictitle"><h2>', $row['item_name'], '</h2></div><br>';
+                echo '<div class="comicprice"><h2>￥', $row['price'], '</h2></div><br>';
+                echo '<div class="volume_number">数量</div>';
+                echo '<div class="wrapper">';
+
+                echo    '<div class="volume_select">';
+                echo        '<select class="volume_select_content" name="count">';
+                echo            '<option value="1">１</option>';
+                echo            '<option value="2">２</option>';
+                echo            '<option value="3">３</option>';
+                echo            '<option value="4">４</option>';
+                echo            '<option value="5">５</option>';
+                echo            '<option value="6">６</option>';
+                echo            '<option value="7">７</option>';
+                echo            '<option value="8">８</option>';
+                echo            '<option value="9">９</option>';
+                echo            '<option value="10">１０</option>';
+                echo        '</select>';
+                echo    '</div>';
+
+                echo '</div>';
+                echo '<input type="hidden" name="item_code" value="', $row['item_code'], '">';
+                echo '<input type="hidden" name="item_name" value="', $row['item_name'], '">';
+                echo '<input type="hidden" name="price" value="', $row['price'], '">';
+                echo '<div class="cart_btn"><input type="submit" value="カートに入れる" id="cart_btn"></div><br>';
+                echo '</form>';
+            }
+            ?>
+            <div class="summary_title"><h3>【作品内容】</h3></div>
+            <div class="line"></div>
+            <div class="comic_summary">
+                WEBで記録的なPVを集めた異世界転生モノの名作を、<br>
+                原作者完全監修でコミカライズ！<br>
+                巻末には原作者書き下ろしの短編小説を収録した、<br>
+                ファン必携の単行本いよいよ発売！
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
