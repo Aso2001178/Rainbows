2d85c9244e2c4d7d253399ca179941788c60fff8
Aso2001178 <83046790+Aso2001178@users.noreply.github.com>
2021-12-07 10:48:19 +0900

    Update customer-input.php

--- ソースコード/customer-input.php
+++ ソースコード/customer-input.php
@@ -3,19 +3,25 @@
     <head>
         <meta charset="UTF-8">
         <title>customer</title>
-        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="./css/customer.css">
     </head>
     <body>
+    <div class="container">
     <form action="customer-out.php" method="post">
+        <div class="pass">
         パスワード : <input type="text" name="pass">
-        <br>
+        </div><br>
+        <div class="name">
         お名前 : <input type="text" name="name">
-        <br>
+        </div><br>
+        <div class="ad">
         住所: <input type="text" name="address">
-        <br>
+        </div><br>
+        <div class="mail">
         メールアドレス: <input type="text" name="mail">
-        <br>
+        </div><br>
         <button type="submit" name="action" value="send">確定</button>
     </form>
+    </div>
     </body>
     </html>
