6683ae51b2dc04a404ea3b103e30e170129fd77c
Aso2001200 <83047043+Aso2001200@users.noreply.github.com>
2021-12-07 10:52:15 +0900

    Update cart.php

--- ソースコード/cart.php
+++ ソースコード/cart.php
@@ -1,38 +1,47 @@
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<title>カート</title>
-	<link rel="stylesheet" href="./css/cartstyle.css">
+    <meta charset="UTF-8">
+    <title>カート</title>
+    <link rel="stylesheet" href="./css/cart.css">
 </head>
 <body>
+<div class="container">
+    <div class="main">
 <?php
 if (!empty($_SESSION['product'])) {
-	echo '<table>';
-	echo '<th>商品番号</th><th>商品名</th>';
-	echo '<th>価格</th><th>数量</th><th>小計</th>';
-	$total=0;
-	foreach ($_SESSION['product'] as $item_code=>$product) {
-		echo '<tr>';
-		echo '<td>', $item_code, '</td>';
-		echo '<td>', $product['item_name'], '</td>';
-		echo '<td>', $product['price'], '</td>';
-		echo '<td>', $product['count'], '</td>';
+    echo '<table>';
+    echo '<th>商品番号</th><th>商品名</th>';
+    echo '<th>価格</th><th>数量</th><th>小計</th>';
+    $total=0;
+    foreach ($_SESSION['product'] as $item_code=>$product) {
+        echo '<tr>';
+        echo '<td>', $item_code, '</td>';
+        echo '<td>', $product['item_name'], '</td>';
+        echo '<td>', $product['price'], '</td>';
+        echo '<td>', $product['count'], '</td>';
         $subtotal=$product['price']*$product['count'];
-		$total+=$subtotal;
-		echo '<td>', $subtotal, '</td>';
-		echo '<td><a href="cart-delete.php?item_code=', $item_code, '">削除</a></td>';
-		echo '</tr>';
-	}
-	echo '<tr><td>合計</td><td></td><td></td><td></td><td>', $total,
-	'</td><td></td></tr>';
-	echo '</table>';
+        $total+=$subtotal;
+        echo '<td>', $subtotal, '</td>';
+        echo '<td><a href="cart-delete.php?item_code=', $item_code, '">削除</a></td>';
+        echo '</tr>';
+    }
+    echo '<tr><td>合計</td><td></td><td></td><td></td><td>', $total,
+    '</td><td></td></tr>';
+    echo '</table>';
 } else {
-	echo 'カートに商品がありません。';
+    echo 'カートに商品がありません。';
 }
 ?>
+    </div>
+    <div class="btn">
+    <div class="button">
 <a href="purchase-input.php">購入手続きへ</a>
+    </div>
+    <div class="button2">
 <a href="toppage.php">トップページへ</a>
+    </div>
+    </div>
+</div>
 </body>
 </html>
