fcffdab1a4dbffa89757c4ccd181d03abcb0a026
Aso2001176 <83047143+Aso2001176@users.noreply.github.com>
2021-12-08 12:43:27 +0900

    Add files via upload

--- ソースコード/login-in.php
+++ ソースコード/login-in.php
@@ -7,18 +7,19 @@
 </head>
 <body>
 <div class="content">
-<h1>AsoSystem</h1>
+    <h1>AsoSystem</h1>
     <div class="control">
         <label for="login"><p>メールアドレス</p></label>
-<form action="login-out.php" method="post">
-    <input type="text" name="mail" id="login">
+        <form action="login-out.php" method="post">
+            <input type="text" name="mail" id="login">
     </div>
     <div class="control">
         <label for="login"><p>パスワード</p></label>
-    <input type="password" name="pass" id="login"required>
+        <input type="password" name="pass" id="login"required>
     </div>
     <p><button type="submit" id="action" value="認証">認証</button></p>
-</form>
+    </form>
 </div>
+<a href="toppage.php">トップページに戻る</a><br>
 </body>
 </html>
