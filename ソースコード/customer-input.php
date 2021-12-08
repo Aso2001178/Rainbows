51a8d4fba8f4261861acdbc68755569b3de5d4c8
Aso2001176 <83047143+Aso2001176@users.noreply.github.com>
2021-12-08 12:42:11 +0900

    Add files via upload

--- ソースコード/customer-input.php
+++ ソースコード/customer-input.php
@@ -3,31 +3,20 @@
     <head>
         <meta charset="UTF-8">
         <title>customer</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Russo+One&display=swap">
-        <link rel="stylesheet" href="css/sanitize.css">
-        <link rel="stylesheet" href="./css/customer.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
-    <div class="container">
-        <div class="icon"><img src="img/本アイコン.png" width="75" height="75"></div>
-        <div class="title" >comic store</div><br>
     <form action="customer-out.php" method="post">
-        <div class="pass">
         パスワード : <input type="text" name="pass">
-        </div><br>
-        <div class="name">
+        <br>
         お名前 : <input type="text" name="name">
-        </div><br>
-        <div class="ad">
+        <br>
         住所: <input type="text" name="address">
-        </div><br>
-        <div class="mail">
+        <br>
         メールアドレス: <input type="text" name="mail">
-        </div><br>
+        <br>
         <button type="submit" name="action" value="send">確定</button>
     </form>
-    </div>
+    <a href="toppage.php">トップページに戻る</a><br>
     </body>
     </html>
