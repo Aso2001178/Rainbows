2529f28ca747dded6f4f51b463d051f99821120f
Aso2001178 <83046790+Aso2001178@users.noreply.github.com>
2021-12-10 10:29:31 +0900

    Update customer-out.php

--- ソースコード/customer-out.php
+++ ソースコード/customer-out.php
@@ -3,9 +3,17 @@
     <head>
         <meta charset="UTF-8">
         <title>customer</title>
-        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Russo+One&display=swap">
+        <link rel="stylesheet" href="css/sanitize.css">
+        <link rel="stylesheet" href="./css/customer.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     </head>
     <body>
+    <div class="container">
+        <div class="icon"><img src="img/本アイコン.png" width="75" height="75"></div>
+        <div class="title" >comic store</div><br>
+        <div class="main">
     <?php
     $pdo=new PDO('mysql:host=mysql147.phy.lolipop.lan;
                 dbname=LAA1291127-rainbows;charset=utf8',
@@ -24,6 +32,10 @@
     echo '<p>登録しました</p>';
     $pdo = null;
     ?>
+        </div>
+        <div class="button">
     <a href="toppage.php">トップページに戻る</a><br>
+        </div>
+    </div>
     </body>
     </html>
